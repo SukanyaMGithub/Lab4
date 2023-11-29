4b2c034e21acec54bdecca395a4c2cfada46e274
SukanyaMGithub <smarati@hawk.iit.edu>
2023-11-28 19:04:17 -0600

    adding tommorrow details in script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Replace 'YOUR_API_KEY' with your actual API key
-const apiKey = 'YOUR_API_KEY';
-
 function getCurrentLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
@@ -14,7 +11,7 @@ function successCallback(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   getSunriseSunsetData(latitude, longitude);
- 
+  getTomorowSunriseSunsetData(latitude, longitude);
 }
 
 function errorCallback(error) {
@@ -34,49 +31,57 @@ function searchLocation() {
   } else {
     showError('Please enter a location.');
   }
+
+  
 }
 
 function hideError() {
   const errorContainer = document.getElementById('errorContainer');
   const resultContainer = document.getElementById('resultContainer');
- 
+  const tomorrowResultContainer = document.getElementById('tomorrowResultContainer');
+
   errorContainer.innerHTML = ''; // Clear the content
   errorContainer.classList.add('hidden');
 
-  
+  // Clear the result container
+  resultContainer.innerHTML = '';
+  tomorrowResultContainer.innerHTML = '';
+  resultContainer.classList.add('hidden');
+  tomorrowResultContainer.classList.add('hidden');
 }
+
 
 
 function getGeocodeData(location) {
-  $.ajax({
-      url: `https://geocode.maps.co/search?q=${location}&format=json`,
-    method: 'GET',
-    success: function (data) {
-      if (data && data.length > 0) {
-        const result = data[0];
-        const match = result.display_name
-        .toLowerCase()
-        .split(',')
-        .some(word => word.trim() === location.toLowerCase());
-        if(match) {
-          const latitude = result.lat;
-        const longitude = result.lon;
-        getSunriseSunsetData(latitude, longitude);
-        getTomorowSunriseSunsetData(latitude, longitude);  
+    $.ajax({
+        url: `https://geocode.maps.co/search?q=${location}&format=json`,
+      method: 'GET',
+      success: function (data) {
+        if (data && data.length > 0) {
+          const result = data[0];
+          const match = result.display_name
+          .toLowerCase()
+          .split(',')
+          .some(word => word.trim() === location.toLowerCase());
+          if(match) {
+            const latitude = result.lat;
+          const longitude = result.lon;
+          getSunriseSunsetData(latitude, longitude);
+          getTomorowSunriseSunsetData(latitude, longitude);  
+          } else {
+            showError('Location not found.');  
+          }
+          
         } else {
-          showError('Location not found.');  
+          showError('Location not found.');
         }
-        
-      } else {
-        showError('Location not found.');
+      },
+      error: function () {
+        showError('Error fetching geocode data.');
       }
-    },
-    error: function () {
-      showError('Error fetching geocode data.');
-    }
-  });
-}
- 
+    });
+  }
+  
 
 function getSunriseSunsetData(latitude, longitude) {
   $.ajax({
@@ -91,23 +96,74 @@ function getSunriseSunsetData(latitude, longitude) {
   });
 }
 
-
+function getTomorowSunriseSunsetData(latitude, longitude) {
+  $.ajax({
+    url: `https://api.sunrisesunset.io/json?lat=${latitude}&lng=${longitude}&date=tomorrow`,
+    method: 'GET',
+    success: function (data) {
+      displayTomorrowSunriseSunsetInfo(data);
+    },
+    error: function () {
+      showError('Error fetching sunrise sunset data.');
+    }
+  });
+}
 
 function displaySunriseSunsetInfo(data) {
   const resultContainer = document.getElementById('resultContainer');
+  
+  
   resultContainer.innerHTML = `
-    <h2>Today</h2>
-    <p>🌅Sunrise: ${data.results.sunrise || 'Not available'}</p>
-    <p>Sunset: ${data.results.sunset || 'Not available'}</p>
-    <p>Dawn: ${data.results.dawn || 'Not available'}</p>
-    <p>Dusk: ${data.results.dusk || 'Not available'}</p>
-    <p>Day Length: ${data.results.day_length || 'Not available'}</p>
-    <p>Solar Noon: ${data.results.solar_noon || 'Not available'}</p>
-    <p>Time Zone: ${data.results.timezone}</p>
-    `;
-    resultContainer.classList.remove('hidden');
+    <div class="day-info">
+      <h4>Today</h4>
+           <div class="sun-info">
+        <span> 🌅<b> Sunrise:</b> ${data.results.sunrise || 'Not available'} <br>
+        🌇 <b>Sunset:</b> ${data.results.sunset || 'Not available'}</span>
+      </div>
+      <div class="sun-info">
+        <span>🌄 <b>Dawn:</b> ${data.results.dawn || 'Not available'}<br> 
+        🌆 <b>Dusk:</b> ${data.results.dusk || 'Not available'}</span>
+      </div>
+      <div class="sun-info">
+        <span>🌞🕒<b> Day Length:</b> ${data.results.day_length || 'Not available'} <br>
+        ☀️🕛 <b>Solar Noon:</b> ${data.results.solar_noon || 'Not available'}</span>   
+      </div>
+      <div class="sun-info">
+        <span>🌐⌚ <b>Time Zone:</b> ${data.results.timezone}</span>
+      </div>
+    </div>
+  `;
+  resultContainer.classList.remove('hidden');
+
+
 }
 
+function displayTomorrowSunriseSunsetInfo(data) {
+
+  const tomorrowResultContainer = document.getElementById('tomorrowResultContainer');
+
+  tomorrowResultContainer.innerHTML = `
+  <div class="day-info">
+    <h4>Tomorrow</h4>
+       <div class="sun-info">
+      <span>🌅 <b>Sunrise:</b> ${data.results.sunrise || 'Not available'}<br>
+      🌇<b> Sunset:</b> ${data.results.sunset || 'Not available'}</span>
+    </div>
+    <div class="sun-info">
+      <span>🌄 <b>Dawn:</b> ${data.results.dawn || 'Not available'}<br>
+      🌆<b> Dusk:</b> ${data.results.dusk || 'Not available'}</span>
+    </div>
+    <div class="sun-info">
+      <span>🌞🕒 <b>Day Length:</b> ${data.results.day_length || 'Not available'}<br>
+      ☀️🕛 <b>Solar Noon:</b> ${data.results.solar_noon || 'Not available'}</span>
+    </div>
+    <div class="sun-info">
+      <span>🌐⌚ <b>Time Zone:</b> ${data.results.timezone}</span>
+    </div>
+  </div>
+`;
+tomorrowResultContainer.classList.remove('hidden');
+}
 
 function showError(message) {
   const errorContainer = document.getElementById('errorContainer');
